418e6dba90ba644d477c4ff66423ecdc7d167f04
hopee0411 <hopee0411@naver.com>
2023-01-03 19:53:30 +0900

    bootStrap install

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+/* eslint-disable prettier/prettier */
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/main.css'
 
 createApp(App).mount('#app')
+
+import 'bootstrap/dist/js/bootstrap.js'
